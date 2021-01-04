2eb487d7727ce7ae0f37a4d8c0e9981fee629ad1
AlexanderMukas <alexandermukas@yahoo.com>
2021-01-05 01:42:59 +0200

    passport config commit

--- passport-config.js
+++ passport-config.js
@@ -1,7 +1,36 @@
 const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
 
+// function authenticateUser()
 
+function initialize(passport, getUserByEmail) {
 
-function initialize(passport) {
+    const authenticateUser = (email, password, done) => {
 
+        const user = getUserByEmail(email);
+        if(user == null) {
+            return done(null, false, { message: 'No user with that email'});
+        }
+        try {
+            if( await bcrypt.compare(password, user.password) ) {
+                return done(null, user);
+            } else {
+                return done(null, false, { message: 'Password incorrect'});
+            }
+        } catch(err) {
+            done(err);
+        }
+    }
+
+    passport.use( new LocalStrategy( {  usernameField: 'email'} ), authenticateUser );
+
+    password.serializeUser( (user, done) => {
+
+    });
+
+    password.deserializeUser( (id, done) => {
+        
+    });
 }
+
+module.exports = initialize;
