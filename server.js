d26dcd02a7be9edd19070eabb230c7dfd9e7b63d
AlexanderMukas <alexandermukas@yahoo.com>
2021-01-05 00:31:45 +0200

    simple page commit

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const app = express();
 
 const PORT = 3000;
 
+app.set('view-engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.render('index.ejs');
+    res.render('index.ejs', { name: 'alex'} )
 });
 
 app.listen(PORT, () => {
     console.log(`Server up on localhost:${PORT} ...`);
-})
+});
